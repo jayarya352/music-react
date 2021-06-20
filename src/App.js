695ff846b3a51399f720de928e9dd0c947a470d0
jagdish chandra <jagdish.appincuba@gmail.com>
2021-06-20 12:31:42 +0530

    fix issues

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import Songs from './pages/Songs';
+import Sidebar from './components/layout/Sidebar'
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import AudioPlayer from './components/layout/AudioPlayer';
 import {BrowserRouter,Route} from 'react-router-dom'
 import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/songs/:name"><Songs/></Route>
-    </Switch>
+    <div id="wrapper" data-scrollable="true">
+      <Sidebar />
+      <main id="pageWrapper">
+        <Header />  
+        <div className="banner bg-home"/>
+        
+          <Switch>
+            <Route exact path="/" ><Home/></Route>
+            <Route exact path="/songs/:name"><Songs/></Route>
+          </Switch>
+        <Footer />
+        <AudioPlayer />
+      </main>
+    </div>
     </BrowserRouter>
   );
 }
